c3381d1af6c99e2cea121511d2a000b37e4ed16c
KitCat0o <KitCat0o@users.noreply.github.com>
2018-01-19 01:05:08 +0300

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from Widget.MainWidget import MainWidget
+
+#adapter
+#command
+#git
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    md = MainWidget()
+    md.show()
+    sys.exit(app.exec_())
